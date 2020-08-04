f7a6abff8e18cf04efc54005853a867bb64b4766
Alexander <shatokhin.04@inbox.ru>
2020-08-04 12:59:26 +0300

    ready 1 2 sections

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots:true,
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/left_1.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/right_1.png"></button>',
+    });
+  });
