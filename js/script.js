7170858f86b30cdee8ec8b0ff01efddbd0cf64b4
Alexander <shatokhin.04@inbox.ru>
2020-08-06 21:57:04 +0300

    all section ready, 1 2 sections adaptive and 3 section adaptive 80%

--- js/script.js
+++ js/script.js
@@ -4,4 +4,39 @@ $(document).ready(function(){
         prevArrow:'<button type="button" class="slick-prev"><img src="icons/left_1.png"></button>',
         nextArrow:'<button type="button" class="slick-next"><img src="icons/right_1.png"></button>',
     });
+
+    $('.descision_carousel').slick({
+        dots:false,
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                dots: true
+              }
+            }
+        ]
+    });
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.m_item'),
+        hamburger = document.querySelector('.hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            })
+        })
+    })
+    
   });
